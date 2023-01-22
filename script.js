d9ac64414a195422831ef7957b55d7eb5ef9ac32
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-01-22 18:18:37 -0500

    WIP: making it work properly

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-const reset = document.querySelector("#reset");
 const tipAmount = document.querySelector("#tip-amount");
 const total = document.querySelector("#total-amount");
-const bill = document.querySelector("#bill");
+const billAmount = document.querySelector("#bill");
+const numberOfPeople = document.querySelector("#number-of-people");
 
-// capture the bill input value
-bill.addEventListener("input", (e) => {
-  // remove the placeholder text
-  e.target.placeholder = "";
-  bill.textContent = `$${e.target.value}`;
-  console.log(bill.textContent);
+let billTotal = 0;
+let tipPercentage = 0;
+let numberOfPeopleTotal = 1;
+
+billAmount.addEventListener("input", (e) => {
+  billAmount.textContent = `$${e.target.value}`;
+  billTotal = Number(e.target.value);
+  return billTotal;
+});
+
+numberOfPeople.addEventListener("input", (e) => {
+  numberOfPeople.textContent = e.target.value;
+  numberOfPeopleTotal = Number(e.target.value);
+
+  return numberOfPeopleTotal;
 });
 
 const percentBtns = document.querySelectorAll(".percentages");
 percentBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    // strip percentage sign
-    const percentage = Number(e.target.value.slice(0, -1));
-    console.log(`🍌`, { percentage }, typeof percentage);
+    tipPercentage = Number(e.target.value.slice(0, -1));
+    calculateTip();
   });
 });
 
-reset.addEventListener("click", () => {
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", reset);
+
+function reset() {
   tipAmount.textContent = "$0.00";
   total.textContent = "$0.00";
   bill.textContent = "$0.00";
-});
+  numberOfPeople.value = 1;
+  billAmount.value = 0;
+}
+
+function calculateTip() {
+  numberOfPeople.value = numberOfPeopleTotal;
+
+  const tip = (billTotal * tipPercentage) / 100;
+  const totalAmount = billTotal + tip;
+  const tipPerPerson = tip / numberOfPeopleTotal;
+  const totalPerPerson = totalAmount / numberOfPeopleTotal;
+
+  tipAmount.textContent = `$${tipPerPerson.toFixed(2)}`;
+  total.textContent = `$${totalPerPerson.toFixed(2)}`;
+}
