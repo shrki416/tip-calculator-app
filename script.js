d2c21c6d962e1ff5cc315291ad8fba844cb443bb
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-01-23 20:51:52 -0500

    WIP

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tipAmount = document.querySelector("#tip-amount");
 const totalAmount = document.querySelector("#total-amount");
+const errorMsg = document.querySelectorAll(".error-msg");
 
 let amount = 0;
 let percentage = 0;
@@ -9,12 +10,53 @@ function convertToNumber(item) {
   return Number(item);
 }
 
+function validateUserInput() {
+  numberOfPeople.value = numberOfPeople.value.replace(/[^0-9]/g, "");
+}
+
+function formatCurrency(amount) {
+  return amount.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+}
+
+function formatNumber(amount) {
+  return amount.toLocaleString("en-US", {
+    maximumFractionDigits: 2,
+  });
+}
+
+function errorCheck(e) {
+  const { id } = e.target;
+  if (e.target.value === "0") {
+    // errorMsg.style.visibility = "visible";
+    errorMsg.forEach((msg) => {
+      console.log(msg.dataset);
+      console.log(id);
+      document.querySelector(`[data-${id}]`).style.visibility = "visible";
+
+      if (msg.dataset === id) {
+        console.log(msg);
+      }
+    });
+    e.target.closest(".input-wrapper").classList.add("error");
+  } else {
+    errorMsg.forEach((msg) => (msg.style.visibility = "hidden"));
+    // errorMsg.style.visibility = "hidden";
+    e.target.closest(".input-wrapper").classList.remove("error");
+  }
+}
+
 const bill = document.querySelector("#bill");
 bill.addEventListener("focus", (e) => (e.target.value = ""));
 bill.addEventListener("input", (e) => getBillAmount(e));
 function getBillAmount(e) {
   if (e.target.value === 0) return;
 
+  errorCheck(e);
+
+  e.target.value = formatNumber(e.target.value.substring(0, 10));
   amount = convertToNumber(e.target.value);
   calculateTip();
 }
@@ -30,7 +72,7 @@ function getPercentage(e) {
     btn.classList.remove("active");
   });
 
-  btn.classList.add("active");
+  e.target.classList.add("active");
 
   percentage = convertToNumber(id);
   calculateTip();
@@ -45,14 +87,21 @@ function getCustomPercentage(e) {
     btn.classList.remove("active");
   });
 
+  e.target.value = e.target.value.substring(0, 5);
+
   percentage = convertToNumber(e.target.value);
   calculateTip();
 }
 
 const numberOfPeople = document.querySelector("#number-of-people");
+numberOfPeople.addEventListener("focus", (e) => (e.target.value = ""));
+numberOfPeople.addEventListener("keyup", validateUserInput);
 numberOfPeople.addEventListener("input", (e) => getNumberOfPeople(e));
 function getNumberOfPeople(e) {
-  if (e.target.value === "" && e.target.value === 0) return;
+  // if (e.target.value === "" && e.target.value === 0) return;
+
+  errorCheck(e);
+
   people = convertToNumber(e.target.value);
   calculateTip();
 }
@@ -61,7 +110,7 @@ const resetBtn = document.querySelector("#reset");
 resetBtn.addEventListener("click", reset);
 function reset() {
   bill.value = 0;
-  numberOfPeople.value = 1;
+  numberOfPeople.value = 0;
   customPercentInput.value = "";
 
   percentageBtns.forEach((btn) => {
@@ -71,10 +120,14 @@ function reset() {
   tipAmount.textContent = "$0.00";
   totalAmount.textContent = "$0.00";
 
+  // errorMsg.style.visibility = "hidden";
+  errorMsg.forEach((msg) => (msg.style.visibility = "hidden"));
+  document.querySelector(".input-wrapper").classList.remove("error");
+
   resetBtn.style.backgroundColor = "var(--inactive)";
 }
 
-function calculateTip() {
+function calculateTip(people = 1) {
   console.log({ amount, percentage, people });
   if (amount === 0 && percentage === 0 && people === 0) {
     resetBtn.style.backgroundColor = "var(--inactive)";
@@ -84,7 +137,10 @@ function calculateTip() {
     const tip = (amount * percentage) / 100;
     const total = amount + tip;
 
-    tipAmount.textContent = `$${(tip / people).toFixed(2)}`;
-    totalAmount.textContent = `$${(total / people).toFixed(2)}`;
+    const tipAmountResult = formatCurrency(tip / people);
+    const totalAmountResult = formatCurrency(total / people);
+
+    tipAmount.textContent = tipAmountResult;
+    totalAmount.textContent = totalAmountResult;
   }
 }
